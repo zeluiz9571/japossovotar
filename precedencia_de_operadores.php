b880bd2d89558bfe72e0d2003ac7c96de1dcbd75
zeluiz9571 <130668873+zeluiz9571@users.noreply.github.com>
2023-09-28 15:41:26 -0300

    É a vida

---
+++ precedencia_de_operadores.php
@@ -0,0 +1,27 @@
+<?php
+
+        $a = 2;
+        $b = 4;
+        $c = 5;
+
+        //Resultado igual 22
+        $calculo = $a + $b * $c;
+        echo $calculo;
+        echo "<br>";
+
+        //Resultado igual a 10
+        $calculo_2 = $b / $a * $c;
+        echo $calculo_2;
+
+        $d = true;
+        $e = false;
+        $f = true;
+
+        //Utilização de 2 operadores lógicos 
+        if ($d || $e && $f) {
+
+            //Condição
+
+
+        }
+?>
