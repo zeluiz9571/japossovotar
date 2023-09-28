3848ebcd86c98695d396315bd848832bd172f0a1
zeluiz9571 <130668873+zeluiz9571@users.noreply.github.com>
2023-09-28 15:33:08 -0300

    Tabela

--- operadores _de_comparacao.php
+++ operadores _de_comparacao.php
@@ -1,69 +1,87 @@
 <?php
-$num1 = 25;
-$num2 = 5;
-$num3 = 5;
+$numero1 = 25;
+$numero2 = 5;
+$numero3 = 5;
 
 //Verifica se $numero1 é igual a $numero2
 if ($numero1 == $numero2) {
     echo "Sim";
+    echo "<br>";
 } else {
     echo "Não";
+    echo "<br>";
 }
 
 //Verifica se $numero1 é diferente de $numero2
 if ($numero1 != $numero2) {
     echo "Sim";
+    echo "<br>";
 } else {
     echo "Não";
+    echo "<br>";
 }
 
 //Verifica se $numero1 é Menor que $numero2
 if ($numero1 < $numero2) {
     echo "Sim";
+    echo "<br>";
 } else {
     echo "Não";
+    echo "<br>";
 }
 
 //Verifica se $numero3 é maior que $numero1
 if ($numero3 > $numero1) {
     echo "Sim";
+    echo "<br>";
 } else {
     echo "Não";
+    echo "<br>";
 }
 
 //Verifica se $numero1 é menor ou igual a $numero2
 if ($numero1 <= $numero2) {
     echo "Sim";
+    echo "<br>";
 } else {
     echo "Não";
+    echo "<br>";
 }
 
 //Verifica se $numero1 é maior ou igual a $numero2
 if ($numero1 >= $numero2){
     echo "Sim";
+    echo "<br>";
 } else {
     echo "Não";
+    echo "<br>";
 }
 
 //Verifica se $numero1 é idêntico a $numero2
 if ($numero3 === $numero2) {
     echo "Sim";
+    echo "<br>";
 } else {
     echo "Não";
+    echo "<br>";
 }
 
 //Verifica se $numero1 não é idêntico a $numero2
 if ($numero1 !== $numero2) {
     echo "Sim";
+    echo "<br>";
 } else {
     echo "Não";
+    echo "<br>";
 }
 
 /*Define o inteiro 1 como resultado da comparação, pois $numero1 é maior que $numero2*/
 if ($numero1 <=> $numero2) {
     echo "Sim";
+    echo "<br>";
 } else {
     echo "Não";
+    echo "<br>";
 }
 
 ?>
