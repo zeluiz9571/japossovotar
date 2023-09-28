7464d595ccffbc0eca6286006345538d13498142
zeluiz9571 <130668873+zeluiz9571@users.noreply.github.com>
2023-09-28 15:29:51 -0300

    8

--- operadores _de_comparacao.php
+++ operadores _de_comparacao.php
@@ -9,3 +9,61 @@ if ($numero1 == $numero2) {
 } else {
     echo "Não";
 }
+
+//Verifica se $numero1 é diferente de $numero2
+if ($numero1 != $numero2) {
+    echo "Sim";
+} else {
+    echo "Não";
+}
+
+//Verifica se $numero1 é Menor que $numero2
+if ($numero1 < $numero2) {
+    echo "Sim";
+} else {
+    echo "Não";
+}
+
+//Verifica se $numero3 é maior que $numero1
+if ($numero3 > $numero1) {
+    echo "Sim";
+} else {
+    echo "Não";
+}
+
+//Verifica se $numero1 é menor ou igual a $numero2
+if ($numero1 <= $numero2) {
+    echo "Sim";
+} else {
+    echo "Não";
+}
+
+//Verifica se $numero1 é maior ou igual a $numero2
+if ($numero1 >= $numero2){
+    echo "Sim";
+} else {
+    echo "Não";
+}
+
+//Verifica se $numero1 é idêntico a $numero2
+if ($numero3 === $numero2) {
+    echo "Sim";
+} else {
+    echo "Não";
+}
+
+//Verifica se $numero1 não é idêntico a $numero2
+if ($numero1 !== $numero2) {
+    echo "Sim";
+} else {
+    echo "Não";
+}
+
+/*Define o inteiro 1 como resultado da comparação, pois $numero1 é maior que $numero2*/
+if ($numero1 <=> $numero2) {
+    echo "Sim";
+} else {
+    echo "Não";
+}
+
+?>
